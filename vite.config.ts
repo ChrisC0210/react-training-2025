3286816391ac6327491b7cd640d4174442b0ea0a
Chris C. <chiandeye@gmail.com>
2025-02-03 03:13:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-training-2025/",
+  // base: "/react-training-2025/",
+  base: './',
   plugins: [react()],
 })
