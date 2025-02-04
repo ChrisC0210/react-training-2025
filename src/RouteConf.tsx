555b08a4731cf9cdb03cf58145d68aeb38917074
Chris C. <chiandeye@gmail.com>
2025-02-04 22:16:52 +0800

    refactor routing configuration to use empty path for root layout

--- src/RouteConf.tsx
+++ src/RouteConf.tsx
@@ -10,7 +10,8 @@ import Week2 from './pages/Week2/layout';
 
 const routes = [
   {
-    path: "/", element: <Layout />,
+    // path: "/", element: <Layout />,
+    path: "", element: <Layout />,
     /// + in the Routes path="/" need to be path=""
     // https://github.com/orgs/community/discussions/36010
     children: [
