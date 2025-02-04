6d6e74dc1a918be955746758e5d3794cc2fd97cd
Chris C. <chiandeye@gmail.com>
2025-02-04 21:24:03 +0800

    add environment configuration for development and production, implement deployment script, and update Vite config for base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "node:url";
 import react from '@vitejs/plugin-react'
+// import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/react-training-2025/",
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/vue-week6/' : './',
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [react()],
 })
